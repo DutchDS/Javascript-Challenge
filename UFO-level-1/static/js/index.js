0acdaf8449efc46eaeeb38439466d9b134759fea
DutchDS <dutchdatasmarts@gmail.com>
2020-01-25 10:04:37 -0600

    works

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -2,16 +2,15 @@
 //##################################
 var submit = d3.select("#submit");
 var get_all = d3.select("#get-all");
-
-// Console.log the UFO data from data.js en set variable
-//######################################################
 var tableData = data;
 
+// Arrow Functions to fill each row and column with UFO Sightings
+//###############################################################
 function bodyOnLoad() {  
-    // Arrow Functions to fill each row and column with UFO Sightings
-    //###############################################################
     var tbody = d3.select("tbody");
       
+    tbody.html(" ");
+
     console.log(tableData);
   
     tableData.forEach((f) => {
@@ -24,75 +23,40 @@ function bodyOnLoad() {
     });
   };
 
-// Define what to do when submit date is clicked
-//##############################################
+// Define what to do when 'get all sightings' date is clicked
+//###########################################################
+get_all.on("click", function() {
+  bodyOnLoad()
+});
+  
+// Define what to do when 'search for specific date' is clicked
+//#################################################################
 submit.on("click", function() {
 
-// //#####prevent the page from refreshing#############
-// //##################################################
-d3.event.preventDefault();
+  d3.event.preventDefault();
+  var inputElement = d3.select("#datetime-form-input");
+  console.log(inputElement);
 
-// //######select the input element and get the raw HTML node##########
-// //##################################################################
-var inputElement = d3.select("#datetime-form-input");
-console.log(inputElement);
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
 
-// //#####get the value property of the input elements#####
-// //######################################################
-var inputValue = inputElement.property("value");
-console.log(inputValue);
-
-// //#####Use form input to filter the date#####
-// //######################################################
-var filteredData = tableData.filter(tableData => {
-  return tableData.datetime == inputValue;
-});
-console.log(filteredData);
+  var filteredData = tableData.filter(tableData => {
+        return tableData.datetime == inputValue;
+      });
+  console.log(filteredData);
 
 // Arrow Functions to fill each row and column with UFO Sightings
 //###############################################################
 var tbody = d3.select("tbody");
 
+tbody.html(" ");
+
 filteredData.forEach((f) => {
-  console.log(f);
-  var row = tbody.append("tr");
-  Object.entries(f).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
+    console.log(f);
+    var row = tbody.append("tr");
+    Object.entries(f).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
 });
-
-// // OR: Use d3 to update each cell's text with
-// data.forEach(function(data) {
-//   console.log(data);
-//   var row = tbody.append("tr");
-//   Object.entries(data).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-});
-
-get_all.on("click", function() {
-  
-  // Arrow Functions to fill each row and column with UFO Sightings
-  //###############################################################
-
-  bodyOnLoad()
-  // var tbody = d3.select("tbody");
-    
-  // console.log(tableData);
-
-  // tableData.forEach((f) => {
-  //   console.log(f);
-  //   var row = tbody.append("tr");
-  //   Object.entries(f).forEach(([key, value]) => {
-  //     var cell = row.append("td");
-  //     cell.text(value);
-  //   });
-  // });
-
-});
-  
