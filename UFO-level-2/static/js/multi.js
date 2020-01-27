53ad574fff5786a516198f0b7b68f8327503441c
DutchDS <dutchdatasmarts@gmail.com>
2020-01-27 14:12:41 -0600

    Dropdown filters working partly

--- UFO-level-2/static/js/multi.js
+++ UFO-level-2/static/js/multi.js
@@ -2,21 +2,13 @@
 //##################################
 var submit_all = d3.select("#submit-all");
 var get_all = d3.select("#get-all");
+var get_state = d3.select("#selectState");
+var get_city = d3.select("#selectCity");
 var tableData = data;
-// var inputValue = d3.select("datetime-form-input")
-var inputDate = d3.select("#select-date-from-list")
-var inputCity = d3.select("#select-city-from-list")
-var inputState = d3.select("#select-state-from-list")
-var inputCountry = d3.select("#select-country-from-list")
-var inputShape = d3.select("#select-shape-from-list")
 
 // Load initial results
 // ####################
 load_all()
-
-// Two functions that will be used on this page
-// ############################################
-
 function load_all() {
     var tbody = d3.select("tbody");
     
@@ -34,49 +26,6 @@ function load_all() {
     });
   }
 
-  function load_select() {
-    d3.event.preventDefault();
-
-    var inputElement = d3.select("#datetime-form-input");
-    console.log(inputElement);
-  
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-  
-    if (!inputValue) {inputValue = '1/1/2010';}
-  
-  
-    var filteredData = tableData.filter(tableData => {
-          return tableData.datetime == inputValue;
-        });
-    console.log(filteredData);
-  
-    // Fill tbody with filteredData
-    //###############################################################
-    var tbody = d3.select("tbody");
-    
-    tbody.html(" ");
-    
-    filteredData.forEach((f) => {
-        console.log(f);
-        var row = tbody.append("tr");
-        Object.entries(f).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
-
-  }
-
-// Define what to do when 'get all sightings' date is clicked
-//###########################################################
-// get_all.on("click", function() {load_all()});
-
-  
-// Define what to do when 'search for specific date' is clicked
-// #################################################################
-// submit_all.on("click", function() {load_select()});
-
 // Create empty arrays to store each list of values
 var listDates = [];
 var listCities = [];
@@ -84,8 +33,7 @@ var listStates = [];
 var listCountries = [];
 var listShapes = [];
 
-// Iterate through each recipe object
-
+// Iterate through each Data record and fill LongLists
 tableData.forEach((r) => {
   listDates.push(r.datetime);
   listCities.push(r.city);
@@ -94,6 +42,7 @@ tableData.forEach((r) => {
   listShapes.push(r.shape)
 })
 
+// Go through each list and create a short list by removing duplicates
 function uniqueValues(myList) {
     var longList = myList;
     var shortList = [];
@@ -121,6 +70,7 @@ console.log(shortStates);
 console.log(shortCountries);
 console.log(shortShapes);
 
+// Fill dropdown boxes when page is refreshed
 function loadDropDowns(myId, myshortList, myText) {
     // var tbody = d3.select("tbody");
     var inputDate = d3.select(myId) 
@@ -137,11 +87,59 @@ function loadDropDowns(myId, myshortList, myText) {
   
       });
     };
-  
 loadDropDowns("#selectDate",shortDates,"Select Date");
 loadDropDowns("#selectCity",shortCities,"Select City");
 loadDropDowns("#selectState",shortStates,"Select State");
 loadDropDowns("#selectCountry",shortCountries,"Select Country");
 loadDropDowns("#selectShape",shortShapes,"Select Shape");
-  
 
+// Lastly Filter result set when a filter is chosen from the dropdownboxes
+  function load_select(myId,myField) {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select(myId);
+    console.log(inputElement);
+  
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+  
+    // if (!inputValue) {inputValue = '1/1/2010';}
+  
+  
+    var filteredData = tableData.filter(tableData => {
+          // if (myField = "State") {
+          // return tableData.state == inputValue};
+          if (myField = "City") {
+          return tableData.city == inputValue};  
+          });
+
+    console.log(filteredData);
+  
+    // Fill tbody with filteredData
+    //###############################################################
+    var tbody = d3.select("tbody");
+    
+    tbody.html(" ");
+    
+    filteredData.forEach((f) => {
+        console.log(f);
+        var row = tbody.append("tr");
+        Object.entries(f).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+  }
+
+get_state.on("click", function() {load_select("#selectState", "State")});
+get_city.on("click", function() {load_select("#selectCity", "City")});
+
+  // Define what to do when 'get all sightings' date is clicked
+//###########################################################
+// get_all.on("click", function() {load_all()});
+
+  
+// Define what to do when 'search for specific date' is clicked
+// #################################################################
+// submit_all.on("click", function() {load_select()});
