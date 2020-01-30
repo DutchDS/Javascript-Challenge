d75dbfd80fac548d239a797e3f287f97c0ade01a
DutchDS <dutchdatasmarts@gmail.com>
2020-01-30 06:26:21 -0600

    Fixed filter to work after multiple changes

--- UFO-Level-1-and-2/static/js/multi.js
+++ UFO-Level-1-and-2/static/js/multi.js
@@ -140,18 +140,66 @@ function load_select(myId, myField) {
       });
     }
 
+function load_all_select() {
+
+  tableData = data;
+  filteredData = tableData
+  // Get all inputElement values
+  let inputValueDate = d3.select("#selectDate").property("value");
+  let inputValueCity = d3.select("#selectCity").property("value");
+  let inputValueState = d3.select("#selectState").property("value");
+  let inputValueCountry = d3.select("#selectCountry").property("value");
+  let inputValueShape = d3.select("#selectShape").property("value");
+
+  console.log(inputValueDate);
+  console.log(inputValueCity);
+  console.log(inputValueState);
+  console.log(inputValueCountry);
+  console.log(inputValueShape);
+
+
+  // For each inputElement where a value has been chosen from its dropdownlist, filter the 
+  if (inputValueDate != "Select Date") {var filteredData = tableData.filter(tableData => {return tableData.datetime == inputValueDate});};
+  tableData = filteredData;
+  if (inputValueCity != "Select City") {var filteredData = tableData.filter(tableData => {return tableData.city == inputValueCity});};
+  tableData = filteredData;
+  if (inputValueState != "Select State")  {var filteredData = tableData.filter(tableData => {return tableData.state == inputValueState});}
+  tableData = filteredData;
+  if (inputValueCountry != "Select Country")  {var filteredData = tableData.filter(tableData => {return tableData.country == inputValueCountry});};
+  tableData = filteredData;
+  if (inputValueShape != "Select Shape")  {var filteredData = tableData.filter(tableData => {return tableData.shape == inputValueShape});};
+  tableData = filteredData;
+
+  console.log(filteredData.length);
+  console.log(filteredData);
+  tableData = filteredData;
+
+  // Fill tbody with filteredData
+  //###############################################################
+  var tbody = d3.select("tbody");
+
+  tbody.html(" ");
+
+  filteredData.forEach((f) => {
+      console.log(f);
+      var row = tbody.append("tr");
+      Object.entries(f).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+}
+
     // Define what to do when value is selected/changed
-get_date.on("change", function() {load_select("#selectDate", "Date")});
-get_city.on("change", function() {load_select("#selectCity", "City")});
-get_state.on("change", function() {load_select("#selectState", "State")});
-get_country.on("change", function() {load_select("#selectCountry", "Country")});
-get_shape.on("change", function() {load_select("#selectShape", "Shape")});
+// get_date.on("change", function() {load_select("#selectDate", "Date")});
+// get_city.on("change", function() {load_select("#selectCity", "City")});
+// get_state.on("change", function() {load_select("#selectState", "State")});
+// get_country.on("change", function() {load_select("#selectCountry", "Country")});
+// get_shape.on("change", function() {load_select("#selectShape", "Shape")});
+get_date.on("change", function() {load_all_select()});
+get_city.on("change", function() {load_all_select()});
+get_state.on("change", function() {load_all_select()});
+get_country.on("change", function() {load_all_select()});
+get_shape.on("change", function() {load_all_select()});
+
 get_all.on("click", function() {location.reload()});
-
-// get_all.on("click", function() {
-//   console.log("Reset was clicked");
-//   load_all()});
-
-// get_reset.on("click", function() {
-//   console.log("Reset was clicked");
-//   load_all()});
